c14dd772225b7c6e51398b2bc8d85ddb6b12baa1
Jeff Hill <grjeff@gmail.com>
2025-10-01 06:42:40 -0400

    Working onboarding with Tailwind

--- components/MobileHeader.tsx
+++
@@ -1,7 +0,0 @@
-export const MobileHeader = () => {
-  return (
-    <header className="bg-white border-b border-gray-200 p-4">
-      <h1 className="text-xl font-bold">Networking BudE</h1>
-    </header>
-  );
-};
